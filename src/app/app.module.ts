fd7d3cc939cf43240741e5fc784ee8815845ffae
keralex <kerlis.aguado@correo.unimet.edu.ve>
2018-10-12 09:49:43 -0600

    inicio pokemon

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PokemonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
