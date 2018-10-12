25db55c2837fe2eb7360b37f078b634a11ba246c
keralex <kerlis.aguado@correo.unimet.edu.ve>
2018-10-12 16:23:06 -0600

    Descripcion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavigationhomeComponent } from './Navigation/navigationhome/navigationhome.component';
+import { DescriptionComponent } from './components/description/description.component';
 
 const routes: Routes = [
    {
     path: '',
     children:[
       { path: '', redirectTo:'/home' , pathMatch: 'full'},
-      {path: 'home',component:HomeComponent}
+      {path: 'home',component:HomeComponent} ,
+      {path: 'description/pokemonid',component:DescriptionComponent}
     ], component: NavigationhomeComponent
 
   }
